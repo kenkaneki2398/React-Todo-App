83b4c1fcdf3019aee4a70a4d11e1c639db3c1112
kenkaneki <lunafreya2398@gmail.com>
2021-02-23 20:56:34 +0700

    Todo-App Structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,34 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>Todo App</h2>
+  const [name, setName] = useState('')
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false)
+  const [editID, setEditID] = useState(null)
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+  return (
+    <section className="section-center">
+      <form className="todo-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>things todo</h3>
+        <div className="form-control">
+          <input type="text" className="todo" placeholder="e.g. check email" value={name} onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="submit-btn">
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      <div className="todo-container">
+        <List />
+        <button className="clear-btn">Clear Items</button>
+      </div>
+    </section>
+  )  
 }
 
 export default App
